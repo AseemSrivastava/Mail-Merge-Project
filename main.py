e1bbf00f77a6d021859e0908dfacc8570f6dff05
Aseem Srivastava <aseemsrivastava9@gmail.com>
2023-01-16 01:35:04 +0530

    Created letter body with name
    Created a letter body using starting_letter text file and added a name to each letter body to whom it is addressed from invited_names text file.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+PLACEHOLDER = "[name]"
+
+#Reading list of names from invited_names file
 with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
-    print(names)
+
+#Reading body of letter from starting_letter file
+with open("./Input/Letters/starting_letter.txt") as letters_file:
+    letter_content = letters_file.read()
+    for name in names:
+        stripped_name = name.strip() #Remove leading and trailing spaces
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        print(new_letter)
